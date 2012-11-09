17b40a976cbf34c85d61c7479cdaa2073177ea13
ldandoy <ldandoy@overconsulting.net>
2012-11-09 14:08:20 +0100

    avancement admin

---
+++ Controller/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace StartPack\AdminBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+use StartPack\CoreBundle\Entity as Model;
+use StartPack\CoreBundle\Form as Form;
+
+use StartPack\CoreBundle\Controller\AbstractCoreController;
+
+
+/**
+ * @Route("/admin/user")
+ */
+class UserController extends AbstractCoreController
+{
+
+	/**
+	 * @Route("/", name="user_index")
+	 * @Template()
+	 */
+    public function indexAction()
+    {
+		$users = $this->getDoctrine()->getRepository('CoreBundle:User')->findAll();
+
+        return array(
+            "users"  =>  $users
+        );
+    }
+    
+}
